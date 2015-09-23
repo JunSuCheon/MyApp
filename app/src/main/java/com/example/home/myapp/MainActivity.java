e50fbd4fd92d7df8f09c3111e64a73682c38d6a1
JunSuCheon <wns8657@naver.com>
2015-09-23 18:03:50 +0900

    First

--- app/src/main/java/com/example/home/myapp/MainActivity.java
+++ app/src/main/java/com/example/home/myapp/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.home.myapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +15,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void button1(View v) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://m.nate.com") );
+        startActivity(intent);
+    }
+
+    public void button2(View v) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:911") );
+        startActivity(intent);
+    }
+
+    public void button3(View v) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("content://media/internal/images/media") );
+        startActivity(intent);
+    }
+
+    public void button4(View v) {
+        finish();
+    }
+
 
 }
