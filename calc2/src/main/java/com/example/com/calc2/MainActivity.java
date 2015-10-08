fc6a4ae29de407720e005c8327fdacbf598e6ee7
com <wwsefd123>
2015-10-08 19:39:23 +0900

    d

---
+++ calc2/src/main/java/com/example/com/calc2/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.com.calc2;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+
+
+
+}
